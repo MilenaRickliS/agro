b549f0cbdb8bef47ef351ab5bff37689094747d8
MilenaRickliS <engs-milenasilverio@camporeal.edu.br>
2024-06-04 22:58:53 -0300

    cc

--- src/pages/Conta/index.jsx
+++ src/pages/Conta/index.jsx
@@ -16,8 +16,8 @@ function Conta() {
         
         <main id="main" class="flexbox-col">
           <h2>Sua Conta</h2>
-          <p>{currentUser.displayName ? currentUser.displayName : currentUser.email} voce está logado</p>
-          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eum corporis, rerum doloremque iste sed voluptates omnis molestias molestiae animi recusandae labore sit amet delectus ad necessitatibus laudantium qui! Magni quisquam illum quaerat necessitatibus sint quibusdam perferendis! Aut ipsam cumque deleniti error perspiciatis iusto accusamus consequuntur assumenda. Obcaecati minima sed natus?</p>
+          <p>Email: {currentUser.displayName ? currentUser.displayName : currentUser.email}</p>
+          
           {
           userLoggedIn
           ?
