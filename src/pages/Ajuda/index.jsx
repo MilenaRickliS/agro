f5f515bcc28c71cb422f8dd8bc86b3742314bb13
MilenaRickliS <engs-milenasilverio@camporeal.edu.br>
2024-06-05 14:42:37 -0300

    cc

--- src/pages/Ajuda/index.jsx
+++ src/pages/Ajuda/index.jsx
@@ -1,22 +1,89 @@
 import "./style.css";
 import Header from "../../components/Header";
 import Footer from "../../components/Footer";
-import React from "react";
+import React, { useState } from "react";
+import emailjs from '@emailjs/browser';
 
-function Ajuda() { 
+function Ajuda() {
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [message, setMessage] = useState('');
+  const [isSubmitted, setIsSubmitted] = useState(false);
+
+  function sendEmail(e) {
+    e.preventDefault();
+
+    if (name === '' || email === '' || message === '') {
+      alert("Preencha todos os campos");
+      return;
+    }
+
+    const templateParams = {
+      from_name: name,
+      message: message,
+      email: email
+    }
+
+    emailjs.send("service_od97lgk", "template_8wi7466", templateParams, "9wOvOMaTj6Bi6wG3M")
+      .then((response) => {
+        console.log("EMAIL ENVIADO", response.status, response.text)
+        setName('')
+        setEmail('');
+        setMessage('');
+        setIsSubmitted(true);
+        setTimeout(() => setIsSubmitted(false), 5000); // Reset 5 seconds
+      }, (err) => {
+        console.log("ERRO: ", err)
+      });
+  }
+
   return (
-      <div>
-        <Header/>
+    <div>
+      <Header />
 
-        
-        <main id="main" class="flexbox-col">
-          <h2>Precisa de Ajuda?</h2>
-          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eum corporis, rerum doloremque iste sed voluptates omnis molestias molestiae animi recusandae labore sit amet delectus ad necessitatibus laudantium qui! Magni quisquam illum quaerat necessitatibus sint quibusdam perferendis! Aut ipsam cumque deleniti error perspiciatis iusto accusamus consequuntur assumenda. Obcaecati minima sed natus?</p>
+      <main id="main" className="flexbox-col">
+        <h2 className="contact_text">Precisa de Ajuda?</h2>
+        <p className="contact_p">Responda ao formulário abaixo</p>
+      </main>
 
-        </main>        
+      <main className="flexbox-col ajuda">
         
-        <Footer/>
-      </div>
+          <div className="container-ajuda">
+            <br />
+            <br />
+            <h1 className="title">Contato</h1>
+            <form className="form" onSubmit={sendEmail}>
+              <input
+                className="input"
+                type="text"
+                placeholder="Digite seu nome"
+                onChange={(e) => setName(e.target.value)}
+                value={name}
+              />
+              <input
+                className="input"
+                type="text"
+                placeholder="Digite seu email"
+                onChange={(e) => setEmail(e.target.value)}
+                value={email}
+              />
+
+              <textarea
+                className="textarea"
+                placeholder="Digite sua mensagem..."
+                onChange={(e) => setMessage(e.target.value)}
+                value={message}
+              />
+                <br/>
+              <input className="ajuda-button" type="submit" value="Enviar" />
+            </form>
+            {isSubmitted && <p style={{ color: 'green' }}>Mensagem enviada com sucesso! Entraremos em contato em breve.</p>}
+          </div>
+       
+      </main>
+
+      <Footer />
+    </div>
   );
 }
 
